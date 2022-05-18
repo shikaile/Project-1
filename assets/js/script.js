a7610481f26f9bd47ccf329c6db167b889464570
史开乐 <csurfssecure@gmail.com>
2022-05-17 22:30:32 -0400

    console log weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,10 +37,25 @@ $ (document).ready(function() {
             //console logs release dates of shoes, should only console log release date of clicked shoe
             // img.onclick = console.log(response.results[i].releaseDate)
         }
-        // console.log(response);
+        console.log(response);
     });
 
 //event listener on click of shoe-options div does not work porperly 
     // $('#shoe-options').click(function(){
     //     console.log(response.results[i].releaseDate)
     // });
+
+    const setting = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://visual-crossing-weather.p.rapidapi.com/history?startDateTime=2019-01-01T00%3A00%3A00&aggregateHours=24&location=Detroit%2CMI%2CUSA&endDateTime=2019-01-03T00%3A00%3A00&unitGroup=us&dayStartTime=8%3A00%3A00&contentType=csv&dayEndTime=17%3A00%3A00&shortColumnNames=0",
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Host": "visual-crossing-weather.p.rapidapi.com",
+            "X-RapidAPI-Key": "4ab2f5ddb3msha930eb28c1b788dp1a54bajsna218795ca125"
+        }
+    };
+    
+    $.ajax(setting).done(function (response) {
+        console.log(response);
+    });

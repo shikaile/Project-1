9f5aeb61db1064db5793f4d7c04ee2bc4d0d6cc4
史开乐 <csurfssecure@gmail.com>
2022-05-18 20:04:06 -0400

    search modals

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,5 +60,34 @@ $ (document).ready(function() {
         console.log(response);
     }
     
-    
+
     );
+    $("#search-btn").click(function(){
+        function showModal(){
+            $('#wearshoe').show()
+            }
+        showModal();
+        
+    
+        function hideModal(){
+            $('#wearshoe').hide()   
+            }
+            $('#close-modal').click(function(){
+            hideModal();
+        })
+        });
+    
+        $("#search-btn").click(function(){
+        function showModal(){
+            $('#wearshoes').show()
+            }
+        showModal();
+        
+    
+        function hideModal(){
+            $('#wearshoe').hide()   
+            }
+            $('#close-modal').click(function(){
+            hideModal();
+        })
+        });

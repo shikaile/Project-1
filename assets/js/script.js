04995153fa539d417c43631dae7f8e0a06fdb8dd
史开乐 <csurfssecure@gmail.com>
2022-06-01 18:46:32 -0400

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,60 @@
+// shoe API constant !!NOTICE CONST FOR SHOE API IS 'settings' WHERE AS CONST FOR WEATHER API IS 'setting'!!
+const settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://v1-sneakers.p.rapidapi.com/v1/sneakers?limit=100",
+    "method": "GET",
+    "headers": {
+        "X-RapidAPI-Host": "v1-sneakers.p.rapidapi.com",
+        "X-RapidAPI-Key": "4ab2f5ddb3msha930eb28c1b788dp1a54bajsna218795ca125"
+    }
+};
 
+//Weather API constant !!NOTICE CONST FOR WEATHER API IS 'setting' WHERE AS CONST FOR SHOE API IS 'settings'!!
+const setting = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://visual-crossing-weather.p.rapidapi.com/history?startDateTime=2019-01-01T00%3A00%3A00&aggregateHours=24&location=Detroit%2CMI%2CUSA&endDateTime=2019-01-03T00%3A00%3A00&unitGroup=us&dayStartTime=8%3A00%3A00&contentType=csv&dayEndTime=17%3A00%3A00&shortColumnNames=0",
+    "method": "GET",
+    "headers": {
+        "X-RapidAPI-Host": "visual-crossing-weather.p.rapidapi.com",
+        "X-RapidAPI-Key": "4ab2f5ddb3msha930eb28c1b788dp1a54bajsna218795ca125"
+    }
+};
+
+const selectedShoe = $("#shoe-options");
+const selectedCity = $("#city-options");
+const shoeSelector = $("#shoe-btn");
+const citySelector = $("#city-btn");
+const shoeRelease = $("#release-date")
+
+//button function for shoe selector button
+$(shoeSelector).click(function(){
+$.ajax(settings).done(function (response) {
+        for (var i = 0; i < response.results.length; i++) {
+            var p = $("<div> ");
+            var img = $('<img>')
+            p.text(response.results[i].releaseDate)
+            img.attr("src", response.results[i].media.smallImageUrl)
+            $('#release-date').append(p);
+            $('#shoe-options').append(img);
+
+            //console logs release dates of shoes, should only console log release date of clicked shoe
+            $("shoe-btn").click(function(){
+                console.log(response.results[i].releaseDate)
+            });
+            // console.log(response.results[i].releaseDate)
+        }
+        console.log(response);
+    });
+});
+
+//button function for shoe selector button
+$(citySelector).click(function(){
+    console.log('working button')
+});
+
+//onLoad/.ready modal for introduction and explanation of site
 $ (document).ready(function() { 
 
     function showModal(){
@@ -15,50 +71,32 @@ $ (document).ready(function() {
     })
 });
 
-    const settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://v1-sneakers.p.rapidapi.com/v1/sneakers?limit=100",
-        "method": "GET",
-        "headers": {
-            "X-RapidAPI-Host": "v1-sneakers.p.rapidapi.com",
-            "X-RapidAPI-Key": "4ab2f5ddb3msha930eb28c1b788dp1a54bajsna218795ca125"
-        }
-    };
-    $.ajax(settings).done(function (response) {
-        for (var i = 0; i < response.results.length; i++) {
-            var p = $("<div>");
-            var img = $('<img>')
-            p.text(response.results[i].releaseDate)
-            img.attr("src", response.results[i].media.smallImageUrl)
-            $('#shoe-options').append(p);
-            $('#shoe-options').append(img);
+// Shoe API constant ('settings') function  !!NOTICE CONST FOR SHOE API IS 'settings' WHERE AS CONST FOR WEATHER API IS 'setting'!!
+// $.ajax(settings).done(function (response) {
+//         for (var i = 0; i < response.results.length; i++) {
+//             var p = $("<div> ");
+//             var img = $('<img>')
+//             p.text(response.results[i].releaseDate)
+//             img.attr("src", response.results[i].media.smallImageUrl)
+//             $('#release-date').append(p);
+//             $('#shoe-options').append(img);
 
-            //console logs release dates of shoes, should only console log release date of clicked shoe
-            // img.onclick = console.log(response.results[i].releaseDate)
-        }
-        console.log(response);
-    });
+//             //console logs release dates of shoes, should only console log release date of clicked shoe
+//             $("shoe-btn").click(function(){
+//                 console.log(response.results[i].releaseDate)
+//             });
+//             // console.log(response.results[i].releaseDate)
+//         }
+//         console.log(response);
+//     });
 
-//event listener on click of shoe-options div does not work porperly 
-    // $('#shoe-options').click(function(){
-    //     console.log(response.results[i].releaseDate)
+// event listener on click of shoe-options div does not work porperly 
+
+
+//Weather API constant ('setting') function !!NOTICE CONST FOR WEATHER API IS 'setting' WHERE AS CONST FOR SHOE API IS 'settings'!!
+    // $.ajax(setting).done(function (response) {
+    //     console.log(response);
     // });
-
-    const setting = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://visual-crossing-weather.p.rapidapi.com/history?startDateTime=2019-01-01T00%3A00%3A00&aggregateHours=24&location=Detroit%2CMI%2CUSA&endDateTime=2019-01-03T00%3A00%3A00&unitGroup=us&dayStartTime=8%3A00%3A00&contentType=csv&dayEndTime=17%3A00%3A00&shortColumnNames=0",
-        "method": "GET",
-        "headers": {
-            "X-RapidAPI-Host": "visual-crossing-weather.p.rapidapi.com",
-            "X-RapidAPI-Key": "4ab2f5ddb3msha930eb28c1b788dp1a54bajsna218795ca125"
-        }
-    };
-    
-    $.ajax(setting).done(function (response) {
-        console.log(response);
-    });
 
 
 $("#search-btn").click(function(){
@@ -71,8 +109,9 @@ $("#search-btn").click(function(){
     function hideModal(){
         $('#wearshoe').hide()   
         }
+
         $('#close-modal').click(function(){
-        hideModal();
+            hideModal();
     })
     });
 
@@ -82,11 +121,10 @@ $("#search-btn").click(function(){
         }
     showModal();
     
-
     function hideModal(){
-        $('#wearshoe').hide()   
+        $('#wearshoes').hide()   
         }
         $('#close-modal').click(function(){
-        hideModal();
-    })
+            hideModal();
+        })
     });
